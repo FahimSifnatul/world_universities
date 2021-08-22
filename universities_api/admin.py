ded42acfa2831432439b51f764826464e3fe6c1f
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-22 22:24:55 +0600

    Created model Universities

--- universities_api/admin.py
+++ universities_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+# customs
+from .models import Universities
+
 # Register your models here.
+admin.site.register(Universities)
--- universities_api/models.py
+++ universities_api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Universities(models.Model):  
+	name = models.CharField(max_length=128)
+	domains = models.CharField(max_length=128)
+	web_pages = models.CharField(max_length=128)
+	country = models.CharField(max_length=64)
+	alpha_two_code = models.CharField(max_length=32)
