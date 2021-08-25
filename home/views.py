6389e86cbf6c2294a3a46dbe7ebea1a76213728a
FahimSifnatul <sifnatul2475@gmail.com>
2021-08-25 22:07:06 +0600

    Added sign in/up form layout

--- home/views.py
+++ home/views.py
@@ -11,16 +11,23 @@ API_URL = 'http://127.0.0.1:8000/'
 # Create your views here.
 class Home(View):  
 	def get(self, request, *args, **kwargs):
-		universities = Universities.objects.all().order_by('country')
-
-		if len(universities) == 0: 
-			# To build the database
-			requests.get(API_URL+'collect')
+		if request.user.is_authenticated:
 			universities = Universities.objects.all().order_by('country')
 
-		context = {}
-		context = {'universities' : universities}
-		return render(request, 'home.html', context)
+			if len(universities) == 0: 
+				# To build the database
+				requests.get(API_URL+'collect')
+				universities = Universities.objects.all().order_by('country')
+
+			context = {}
+			context['user_authenticated'] = 'True'
+			context['universities'] = universities
+			return render(request, 'home.html', context)
+
+		else:  
+			context = {}
+			context['user_authenticated'] = 'False'
+			return render(request, 'home.html', context)
 
 
 	def post(self, request, *args, **kwargs):  
